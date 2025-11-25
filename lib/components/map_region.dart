8409ee70d9e2b60c16046a7bb597ed7d2287e5dc
v.yaremchuk@admin.idknet.com <v.yaremchuk@admin.idknet.com>
2025-11-25 14:28:47 +0200

    Add objects array to map region

--- lib/components/map_region.dart
+++ lib/components/map_region.dart
@@ -3,10 +3,13 @@ import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 
+import 'tile_objects/tile_object.dart';
+
 class MapRegion extends IsometricTileMapComponent with TapCallbacks {
   int xPos;
   int yPos;
   bool visible = true;
+  late final List<List<TileObject?>> objects;
 
   MapRegion(super.tileset, super.matrix, this.xPos, this.yPos)
     : super(destTileSize: Vector2.all(64), tileHeight: 16) {
@@ -15,6 +18,10 @@ class MapRegion extends IsometricTileMapComponent with TapCallbacks {
       matrix.length * yPos,
     );
     priority = xPos + yPos;
+    objects = List.generate(
+      matrix.length,
+      (_) => List.generate(matrix[0].length, (_) => null),
+    );
   }
 
   @override
@@ -28,6 +35,6 @@ class MapRegion extends IsometricTileMapComponent with TapCallbacks {
     if (!containsBlock(block) || blockValue(block) == -1) {
       return;
     }
-    //TODO handle by object
+    objects[block.y][block.x]?.onTap();
   }
 }
---
+++ lib/components/tile_objects/tile_object.dart
@@ -0,0 +1,54 @@
+import 'package:flame/components.dart';
+
+import '../map_region.dart';
+
+abstract class TileObject extends PositionComponent with ParentIsA<MapRegion> {
+  UnionType unionType;
+  late Block _pos;
+
+  TileObject(Block position, this.unionType) : super() {
+    _pos = position;
+  }
+
+  @override
+  void onMount() {
+    super.onMount();
+    position = parent.getBlockRenderPosition(_pos);
+    priority = _pos.x + _pos.y;
+    parent.objects[_pos.y][_pos.x] = this;
+  }
+
+  @override
+  void onRemove() {
+    super.onRemove();
+    parent.objects[_pos.y][_pos.x] = null;
+  }
+
+  Block get pos => _pos;
+  set pos(Block value) {
+    _pos = value;
+    position = parent.getBlockRenderPosition(value);
+    priority = value.x + value.y;
+  }
+
+  void onTap();
+}
+
+enum UnionType {
+  nothing,
+  n,
+  e,
+  s,
+  w,
+  ns,
+  we,
+  ne,
+  se,
+  nw,
+  sw,
+  wne,
+  nes,
+  wse,
+  nws,
+  full,
+}
